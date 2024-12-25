a1bb57874c16a546c0b1bd3200628550c4dabb48
Enayatullah Norozi <enayathushmand12@gmail.com>
2024-12-25 14:19:14 +0100

    Solve Day09, part 2

--- src/Day09.hs
+++ src/Day09.hs
@@ -2,8 +2,8 @@ module Day09 where
 
 import Utils
 import Data.Char
-import Debug.Trace
 import Data.List
+import Data.Function
 
 type ID = Int
 data Block = Free | File ID deriving (Show, Eq, Ord)
@@ -42,10 +42,46 @@ part1 inp = do
 compress :: (Eq a, Ord a) => [a] -> [(Int, a)]
 compress xs = map (\(x:xs) -> (length xs + 1, x)) $ group xs
 
+moveLeft :: (Int, Block) -> [(Int, Block)] -> Maybe [(Int, Block)]
+moveLeft b (x:_) | x == b = Nothing
+moveLeft b@(size, _) (x@(sizeX, Free):xs)
+  | sizeX >= size = let free = sizeX - size in Just $ if free > 0 then b:(free, Free):xs else b:xs
+moveLeft b (x:xs) = fmap (x:) $ moveLeft b xs
+
+freeRight :: (Int, Block) -> [(Int, Block)] -> [(Int, Block)]
+freeRight e@(size, _) xs = reverse $ go (reverse xs)
+  where
+    go [] = []
+    go (x:xs) | x == e = (size, Free):xs
+    go (x:xs) = x:go xs
+
+mergeFrees :: [(Int, Block)] -> [(Int, Block)]
+mergeFrees = concatMap merge . groupBy ((==) `on` snd)
+  where
+    merge xs@((_, Free):_) = [(sum (map fst xs), Free)]
+    merge xs = xs
+
+compactDisk' :: [(Int, Block)] -> [(Int, Block)]
+compactDisk' xs = foldr moveBlock xs xs
+  where
+    moveBlock x acc = case moveLeft x acc of
+      Just xs -> mergeFrees $ freeRight x xs
+      Nothing -> acc
+
+decompress :: [(Int, Block)] -> [Block]
+decompress = concatMap (\(size, b) -> replicate size b)
+
+toString :: [Block] -> String
+toString = map showBlock 
+  where
+    showBlock Free = '.'
+    showBlock (File i)
+      | i < 10 = intToDigit i
+      | otherwise = '#'
 
 part2 :: String -> IO ()
 part2 inp = do
   let expanded = expandDisk (strip inp)
       compressed = compress expanded
-  print inp
-  print compressed
+      blocks = compactDisk' compressed
+  print $ checksum $ decompress blocks
